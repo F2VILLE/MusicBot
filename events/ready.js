00e2e7b2053e3003e5323f2e063a7e1dca045179
F2Ville <feliciendeville@gmail.com>
2023-05-18 20:57:58 +0200

    Fixing everyone permission

--- events/ready.js
+++ events/ready.js
@@ -11,7 +11,13 @@ module.exports = {
         const commands = client.commands.map(x => x.options)
 
         client.guilds.cache.forEach(guild => {
-            guild.commands.set(commands).then(() => {
+
+            guild.commands.set(commands.map(x => {
+                if (!x.permission) {
+                    x["permission"] = guild.id
+                }
+                return x
+            })).then(() => {
                 console.log("[+] Commands setup for " + guild.name)
             }).catch(e => {
                 console.log("[-] Error setting commands for " + guild.name)
